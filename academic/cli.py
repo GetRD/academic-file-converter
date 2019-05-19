9c911a791f99358f427bbb7a3a8d6718c625adbb
Stewart Jamieson <SJamieson@users.noreply.github.com>
2019-05-18 22:03:40 -0400

    feat: sync support for phdthesis, masterthesis, and patent (#38)
    Upstream, Academic added dedicated Thesis and Patent types.
    
    This commit syncs with those upstream changes.

--- academic/cli.py
+++ academic/cli.py
@@ -31,12 +31,13 @@ PUB_TYPES = {
     'incollection': 6,
     'inproceedings': 1,
     'manual': 4,
-    'mastersthesis': 4,
+    'mastersthesis': 7,
     'misc': 0,
-    'phdthesis': 4,
+    'phdthesis': 7,
     'proceedings': 0,
     'techreport': 4,
-    'unpublished': 3
+    'unpublished': 3,
+    'patent': 8
 }
 
 
