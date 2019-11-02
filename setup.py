8edfe518fe6351837668c8eaeccfd9efa792478e
George Cushen <gcushen@users.noreply.github.com>
2019-11-02 15:20:59 +0000

    refactor: version location

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import setup, find_packages
 
 def get_version(package):
     """Return package version as listed in `__version__` in `init.py`."""
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+    version_py = open(os.path.join(package, 'version.py')).read()
+    return re.search("version = ['\"]([^'\"]+)['\"]", version_py).group(1)
 
 
 version = get_version('academic')
