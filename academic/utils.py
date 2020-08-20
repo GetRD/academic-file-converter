6b3c49ae7a0cfc8ccdf5e387eb57b4fc6c8dc412
Alexander Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-08-20 16:49:30 +0100

    fix: check for docker-compose (#71)
    Path().glob("docker-compose.yml") returns a generator that is always a
    true value in if conditions (even if the list that it returns is emtpy).
    Fix this by using `Path("docker-compose.yml").exists()`.

--- academic/utils.py
+++ academic/utils.py
@@ -5,7 +5,7 @@ def hugo_in_docker_or_local():
     """Checks if there's a `docker-compose.yml` in local directory. If so, prefer that
     to a local hugo installation.
     """
-    if Path().glob("docker-compose.yml"):
+    if Path("docker-compose.yml").exists():
         hugo = "docker-compose run hugo"
     else:
         hugo = ""
